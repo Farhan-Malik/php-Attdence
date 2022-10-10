08c7838a725f305d032a12e9c08832a1cc698063
Farhan Malik <farhanmalik.maa@gmail.com>
2022-10-11 00:06:09 +0530

    users php edit

--- db/connect.php
+++ db/connect.php
@@ -1,30 +1,33 @@
 <?php
-   //Devlopment Connection
-   //$host = 'localhost';
-   //$db = 'Registration_db';
-   //$user = 'root';
-   //$pass = '';
-   //$charset = 'utf8mb4';
+//Devlopment Connection
+$host = 'localhost';
+$db = 'Registration_db';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
 
-   //Remote Database connection
-   $host = 'sql6.freemysqlhosting.net';
-   $db = 'sql6525343';
-   $user = 'sql6525343';
-   $pass = 'JMev7Uv1KR';
-   $charset = 'utf8mb4';
-
-
-   $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-   
-   try{
-       $pdo = new PDO($dsn, $user, $pass);
-       $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    } catch(PDOException $e) {
-       throw new PDOException($e->getMessage());
-   }
+//Remote Database connection
+//$host = 'sql6.freemysqlhosting.net';
+//$db = 'sql6525343';
+//$user = 'sql6525343';
+//$pass = 'JMev7Uv1KR';
+//$charset = 'utf8mb4';
 
 
-   require_once 'crud.php';
-   $crud = new crud($pdo);
-?> 
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+try {
+   $pdo = new PDO($dsn, $user, $pass);
+   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+   throw new PDOException($e->getMessage());
+}
+
+
+require_once 'crud.php';
+require_once 'user.php';
+$crud = new crud($pdo);
+$user = new users($pdo);
+
+$user->insertUser("admin","password");
+?>
---
+++ db/user.php
@@ -0,0 +1,68 @@
+<?php
+class crud
+{
+    private $db;
+
+    function __construct($connect)
+    {
+        $this->db = $connect;
+    }
+
+    public function insertUser($username, $password)
+    {
+        try {
+            $result = $this->getUserbyUsername($username);
+            if ($result['num'] > 0) {
+                return false;
+            } else {
+                $new_password = md5($password.$username);
+                $sql = "INSERT INTO users (username,password) VALUES (:username,:password)";
+                //prepare the sql statement for execution
+                $stmt = $this->db->prepare($sql);
+                // bind all placeholders to this actual values
+                $stmt->bindparam(':username', $username);
+                $stmt->bindparam(':password', $new_password);
+
+                // execute statement 
+                $stmt->execute();
+                return true;
+            }
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            return false;
+        }
+    }
+
+    public function getUser($username, $password)
+    {
+
+        try {
+            $sql = "select * from users where username = :username AND password = :password ";
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindparam(':username', $username);
+            $stmt->bindparam(':password', $password);
+            $stmt->execute();
+            $result = $stmt->fetch();
+            return $result;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            return false;
+        }
+    }
+
+    public function getUserbyUsername($username)
+    {
+        try {
+            $sql = "select count(*) as num from users where username = :username";
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindparam(':username', $username);
+
+            $stmt->execute();
+            $result = $stmt->fetch();
+            return $result;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            return false;
+        }
+    }
+}
